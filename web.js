dd5f5527859cbf8012d3cbbd5524e3aee661e339
Bill Lee <chernhsing.lee@gmail.com>
2013-07-22 11:40:24 +0000

    redo bug fix 1 web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ app.use(express.logger());
 app.get('/', function(request, response) {
     var fs = require('fs');
     var buffer = require('buffer');
-    var index_buffer = fs.readFileSync(index.html);
+    var index_buffer = fs.readFileSync('index.html');
     response.send(buffer.toString(index_buffer));
 });
 

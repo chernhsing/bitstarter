83c3398117224ad82d27a284d4f4b45b86e7642c
Bill Lee <chernhsing.lee@gmail.com>
2013-07-23 03:27:12 +0000

    redo bug fix web.js

--- web.js
+++ web.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var fs = require('fs');
+var buffer = require('buffer');
 var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
     var index_buffer;
     index_buffer = fs.readFileSync('index.html');
-    response.send(index_buffer.toString(index_buffer));
+    response.send(buffer.toString(index_buffer));
 });
 
 var port = process.env.PORT || 5000;
